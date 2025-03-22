cc56a9385333a3c155ee6dade4d1966a38284e30
sam nikzad <abbas.nikzad70@gmail.com>
2025-03-22 20:11:45 +0330

    lesson 05 added

---
+++ backups-for-course/lesson-05/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 8000
+const server = app.listen(PORT,()=>{
+    console.log(`chat server on port : ${PORT} `)
+})
+
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+let socketConnected = new Set()
+
+io.on('connection',onConnected)
+
+function onConnected(socket){
+    console.log(`socket connected : ${socket.id}`)
+    socketConnected.add(socket.id)
+
+    io.emit('client-total', socketConnected.size)
+
+    socket.on('disconnect',()=>{
+        console.log(`socket disconnected ${socket.id}`)
+        socketConnected.delete(socket.id)
+        io.emit('client-total', socketConnected.size)
+    })
+
+    socket.on('message',(data)=>{
+        socket.broadcast.emit('chat-message',data)
+    })
+}
+
+
+
